7f1ba4757d2646a04b9fe18a1a8724315238042c
Ariadna Maldonado <ariadna.maldonado@modo.com.ar>
2025-07-04 17:10:53 -0300

    update role for users

--- librosclub-backend/src/app.js
+++ librosclub-backend/src/app.js
@@ -33,6 +33,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Configurar rutas
+app.use('/api', protectedRoutes);
 app.use('/api/auth', authRoutes); // Rutas de autenticación
 app.use('/api/books', booksRoutes); // Rutas de libros
 app.use('/api/protected', protectedRoutes); // Rutas protegidas
--- librosclub-backend/src/routes/protected.js
+++ librosclub-backend/src/routes/protected.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const authMiddleware = require('../middlewares/authMiddleware');
+const allowRoles = require('../middlewares/roleMiddleware');
 const router = express.Router();
 
 // Ruta protegida
 router.get('/protected', authMiddleware, (req, res) => {
   res.json({ message: 'Acceso permitido', user: req.user });
 });
+
+// Ruta solo para admins
+router.get('/admin-only', authMiddleware, allowRoles('admin'), (req, res) => {
+  res.json({ message: `Bienvenido admin ${req.user.username}` });
+});
+
 
 module.exports = router;
